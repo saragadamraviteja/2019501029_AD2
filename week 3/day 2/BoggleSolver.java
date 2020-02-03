a27859cc62d012a76dd56b4f24407ffa8ee0154c
saragadamraviteja <saragadamraviteja@msitprogram.net>
2020-02-03 13:49:27 +0530

    final

--- week 3/day 2/BoggleSolver.java
+++ week 3/day 2/BoggleSolver.java
@@ -1,4 +1,7 @@
-import edu.princeton.cs.algs4.*;
+import java.util.HashSet;
+
+// import edu.princeton.cs.algs4.*;
+import java.util.Set;
 /**
  * BoggleSolver
  */
@@ -6,18 +9,93 @@ public class BoggleSolver {
 
     // Initializes the data structure using the given array of strings as the dictionary.
     // (You can assume each word in the dictionary contains only the uppercase letters A through Z.)
-    TrieST tt;
+    private TrieSET tt;
 
     public BoggleSolver(String[] dictionary) {
-        tt = new TrieST<Integer>();
+        if (dictionary == null) {
+            throw new IllegalArgumentException();
+        }
+
+        tt = new TrieSET();
+
         for (int i = 0; i < dictionary.length; i++) {
-                tt.put(dictionary[i], scoreOf(dictionary[i]));
+                tt.add(dictionary[i]);
         }
      }
 
+    //  String[] validWords;
+    private int count = 0;
+     private void testing(int k, int l, BoggleBoard board) {
+        count ++;
+        if (k < 0 || l < 0|| k >= board.rows()|| l >= board.cols()) {
+            return;
+        }
+
+        if (bool[k][l] == false) {
+
+            bool[k][l] = true;
+
+            if (board.getLetter(k , l) == 'Q') {
+                str.append(Character.toString(board.getLetter(k, l)));
+
+                str.append(Character.toString('U'));
+            } else {
+                str.append(Character.toString(board.getLetter(k, l)));
+            }
+
+        if (str.length() >= 3) {
+
+            if (tt.contains(str.toString())) {
+                valid.add(str.toString());
+            }
+
+            if (tt.hasPrefix(str.toString())== false) {
+
+                if (board.getLetter(k, l) == 'Q') {
+                    
+                    str.delete(str.length()-2, str.length());
+                } else {
+
+                    str.delete(str.length()-1, str.length());
+                }
+
+                bool[k][l] = false;
+                return;
+            }
+        }
+
+        for(int i =-1; i <2; i++ )  {
+            for(int j = -1; j< 2; j++) {
+                    testing(k + i, l+j,board);
+            }
+        }
+        if (board.getLetter(k, l) == 'Q') {
+                    
+            str.delete(str.length()-2, str.length());
+        } else {
+
+            str.delete(str.length()-1, str.length());
+        }
+        bool[k][l] = false;
+        }
+        return;
+     }
+
     // Returns the set of all valid words in the given Boggle board, as an Iterable.
+    private Set<String> valid;
+    private StringBuffer str;
+    private boolean[][] bool;
     public Iterable<String> getAllValidWords(BoggleBoard board) {
-            
+            valid = new HashSet<String>();
+            // Set<String> valid = new HashSet<String>();
+            bool = new boolean[board.rows()][board.cols()];
+            str = new StringBuffer();
+            for (int k = 0; k < board.rows(); k++) {
+                for(int l = 0; l < board.cols(); l++) {
+                    testing(k,l,board);
+                }
+            }
+        return valid;
     }
 
     // Returns the score of the given word if it is in the dictionary, zero otherwise.
@@ -40,8 +118,8 @@ public class BoggleSolver {
     } 
 
     public static void main(String[] args) {
-        In in = new In(args[0]);
-        String[] dictionary = in.readAllStrings();
-        BoggleSolver solver = new BoggleSolver(dictionary);
+        // In in = new In(args[0]);
+        // String[] dictionary = in.readAllStrings();
+        // BoggleSolver solver = new BoggleSolver(dictionary);
     }
 }
